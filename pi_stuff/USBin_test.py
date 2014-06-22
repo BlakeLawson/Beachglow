884ef33916a486079e24b31a8a75131314bb2d25
Blake Lawson <blawson@princeton.edu>
2014-06-22 15:12:56 -0400

    Made some changes to the USBin file.

--- pi_stuff/USBin_test.py
+++ pi_stuff/USBin_test.py
@@ -2,16 +2,29 @@ import usb.core
 import usb.util
 
 dev = usb.core.find()
+interface = 0
+endpoint = dev[0][(0,0)][0]
+address = endpoint.bEndpointAddress
+size = endpoint.wMaxPacketSize
 
 if dev is None:
 	raise ValueError('Device not found.')
-
-dev.detach_kernel_driver(0)
+try:
+	dev.detach_kernel_driver(interface)
+except:
+	pass
 
 dev.set_configuration()
+usb.util.claim_interface(dev, interface)
+# cfg = dev.get_active_configuration()
+# intf = cfg[(0,0)]
 
-cfg = dev.get_active_configuration()
-intf = cfg[(0,0)]
+
+# while True:
+# 	data = dev.read(0x02, 0x0020)
+# 	print(data)
+
+
 
 # Dependencies usblib (do 'sudo apt-get install usblib-1.0')
 # then git clone https://github.com/walac/pyusb.git
