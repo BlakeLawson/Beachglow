b28223094529388591d71aa9d3c2e84c730d58fd
Blake Lawson <blawson@princeton.edu>
2014-06-28 18:53:14 -0400

    Decided to use external num pad for input, so messing with reading input

---
+++ pi_stuff/keypad_logic.py
@@ -0,0 +1,39 @@
+from getch import _Getch
+import keyboardleds
+import commands
+import sys
+
+keyPress = _Getch()
+#device_path = '/dev/input/by-id/usb-099a_USB_Keypad-event-kbd'
+#device_path = '/dev/input/by-path/pci-0000:00:12.0-usb-0:1:1.0-event-kbd'
+#device_path = '/dev/input/by-path/pci-0000:00:13.2-usb-0:5:1.0-event'
+#ledkit = keyboardleds.LedKit(device_path)
+
+
+
+while True:
+	# ledkit.num_lock.set()
+	#print ledkit.num_lock.get()
+
+
+	# lock_status = commands.getoutput('xset q | grep LED')[65]
+
+	# if lock_status == '1':
+	# 	print "Cap lock on."
+	# elif lock_status == '0':
+	# 	print "Num lock off."
+	# elif lock_status == '2':
+	# 	print "numlock"
+
+	# raw_input()
+
+
+	key = keyPress()
+	if key == ';':
+		break
+	elif key == u'001B':
+		print "in elif"
+		rest = sys.stdin.read(2)
+		print rest
+
+	print key
