7b38f0ba0c0a086ce00b0168adaaa3fc9161e737
Marco Giancarli <m.a.giancarli@gmail.com>
2014-07-04 04:59:22 -0400

    added sales view and template

--- sales_record/views.py
+++ sales_record/views.py
@@ -1,32 +1,49 @@
 from django.shortcuts import render_to_response
-from chartit import DataPool, Chart
+#from chartit import DataPool, Chart
+from datetime import datetime
+import time
 from sales_record.models import Sale
 
 def display_results(request):
-	# Filter 
+	# Filter
 	#saleObjects = Sale.objects.all()
+    #
+    #
+    #
+    #data = DataPool(series=[{
+    #    'options': {'source': Sale.objects.all()},
+    #    'terms': ['product', 'time']
+    #}])
+    #
+    #chart = Chart(
+    #    datasource=data,
+	#    series_options=[{
+    #        'options':{
+    #            'type':'line',
+    #            'stacking':False},
+    #        'terms':{
+    #            'product':['time',],}
+    #    }],
+    #    chart_options={
+    #        'title':{'text':'Water Bottles Sales'},
+    #        'xAxis':{
+    #            'title':{
+    #                'text':'Time (Minutes) Since Noon'}}
+    #        'yAxis':{
+    #            'title':{
+    #                'text':'Water Bottles Sold'}}
+    #})
+    #
 
-	
+    base_datetime = datetime(2014,7,5,12,0,0)
+    minutes = range(0,660,10)
+    sales = [0 for m in minutes]
+    all_sales = Sale.objects.all()
 
-	data = DataPool(series=[{
-		'options': {'source': Sale.objects.all()},
-		'terms': ['product', 'time']
-	}])
+    for sale in all_sales:
+        diff = time.mktime(sale.time.timetuple()) - time.mktime(base_datetime.timetuple())
+        minute = int(diff / 600)
+        if 0 < minute < 66:
+            sales[minute] += 1
 
-	chart = Chart(
-		datasource=data,
-		series_options=[{
-			'options':{
-				'type':'line',
-				'stacking':False},
-			'terms':{
-				'product':['time',],}
-		}],
-		chart_options={
-			'title':{'text':'Inventory Sold vs Time Sold'},
-			'xAxis':{
-				'title':{
-					'text':'Time of Sale'}}
-	})
-
-	return render_to_response('index.html', {'chart':chart},)
+    return render_to_response('index.html', {'sales':sales, 'minutes':minutes},)
