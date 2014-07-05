4246137a471aa1fda4f4b939f1ddd092e8f87f9d
Blake Lawson <blawson@princeton.edu>
2014-07-05 07:56:13 -0400

    Final updates

--- sales_record/views.py
+++ sales_record/views.py
@@ -1,40 +1,10 @@
-from django.shortcuts import render_to_response
-#from chartit import DataPool, Chart
-from datetime import datetime
 import time
+
+from django.shortcuts import render_to_response
+from datetime import datetime
 from sales_record.models import Sale
 
 def display_results(request):
-	# Filter
-	#saleObjects = Sale.objects.all()
-    #
-    #
-    #
-    #data = DataPool(series=[{
-    #    'options': {'source': Sale.objects.all()},
-    #    'terms': ['product', 'time']
-    #}])
-    #
-    #chart = Chart(
-    #    datasource=data,
-	#    series_options=[{
-    #        'options':{
-    #            'type':'line',
-    #            'stacking':False},
-    #        'terms':{
-    #            'product':['time',],}
-    #    }],
-    #    chart_options={
-    #        'title':{'text':'Water Bottles Sales'},
-    #        'xAxis':{
-    #            'title':{
-    #                'text':'Time (Minutes) Since Noon'}}
-    #        'yAxis':{
-    #            'title':{
-    #                'text':'Water Bottles Sold'}}
-    #})
-    #
-
     base_datetime = datetime(2014,7,5,12,0,0)
     minutes = range(0,660,10)
     sales = [0 for m in minutes]
@@ -43,7 +13,7 @@ def display_results(request):
     for sale in all_sales:
         diff = time.mktime(sale.time.timetuple()) - time.mktime(base_datetime.timetuple())
         minute = int(diff / 600)
-        if 0 < minute < 66:
+        if 0 < minute < 660:
             sales[minute] += 1
         else:
             sales[0] += 1
