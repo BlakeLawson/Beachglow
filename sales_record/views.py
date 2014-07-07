ce886b67595e5f0ffc2bef6116f7be579dce40bb
Blake Lawson <blawson@princeton.edu>
2014-07-06 20:09:45 -0400

    Updated to account for data

--- sales_record/views.py
+++ sales_record/views.py
@@ -5,15 +5,15 @@ from datetime import datetime
 from sales_record.models import Sale
 
 def display_results(request):
-    base_datetime = datetime(2014,7,5,12,0,0)
-    minutes = range(0,660,10)
+    base_datetime = datetime(2014,7,4,8,0,0)
+    minutes = range(0,400,10)
     sales = [0 for m in minutes]
     all_sales = Sale.objects.all()
 
     for sale in all_sales:
         diff = time.mktime(sale.time.timetuple()) - time.mktime(base_datetime.timetuple())
         minute = int(diff / 600)
-        if 0 < minute < 660:
+        if 0 < minute < 400:
             sales[minute] += 1
         else:
             sales[0] += 1
